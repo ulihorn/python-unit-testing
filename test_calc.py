0d82da6e2108230494a4d82f8e865821312d594b
Uli Horn <ulihorn@gmail.com>
2018-08-19 10:17:11 +0200

    added travis file

---
+++ test_calc.py
@@ -0,0 +1,13 @@
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 1)
+        self.assertEqual(calc.add(-1, -1), -2)
+
+
+if __name__ == '__main__':
+    unittest.main()
