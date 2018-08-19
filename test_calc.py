42515426ca4ab5b68deab53ee681c3fd08d502e4
Uli Horn <ulihorn@gmail.com>
2018-08-19 12:08:12 +0200

    fix test

--- test_calc.py
+++ test_calc.py
@@ -5,7 +5,7 @@ class TestCalc(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(calc.add(10, 5), 15)
-        self.assertEqual(calc.add(-1, 1), 1)
+        self.assertEqual(calc.add(-1, 1), 0)
         self.assertEqual(calc.add(-1, -1), -2)
         self.assertEqual(calc.add(1, 1), 2)
 
