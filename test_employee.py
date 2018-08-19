7e93d523ffe53d7094c26b67a619bbdaffee17c3
Uli Horn <ulihorn@gmail.com>
2018-08-19 19:21:41 +0200

    add class methods to employee class

--- test_employee.py
+++ test_employee.py
@@ -3,14 +3,25 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
     def setUp(self):
+        print('setup')
         self.emp_1 = Employee('Corey', 'Schafer', 50000)
         self.emp_2 = Employee('Sue', 'Smith', 60000)
 
     def tearDown(self):
+        print('teardown')
         pass
 
     def test_email(self):
+        print('test_email')
         self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
         self.assertEqual(self.emp_2.email, 'Sue.Smith@email.com')
 
@@ -21,16 +32,18 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
 
     def test_fullname(self):
+        print('test_fullname')
         self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
         self.assertEqual(self.emp_2.fullname, 'Sue Smith')
 
-        self.emp_1.first = 'Johnny'
+        self.emp_1.first = 'John'
         self.emp_2.first = 'Jane'
 
         self.assertEqual(self.emp_1.fullname, 'John Schafer')
         self.assertEqual(self.emp_2.fullname, 'Jane Smith')
 
     def test_apply_raise(self):
+        print('test_apply_raise')
         self.emp_1.apply_raise()
         self.emp_2.apply_raise()
 
